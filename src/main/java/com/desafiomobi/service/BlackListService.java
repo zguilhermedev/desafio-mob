1339019c14f5c746157a198b423f29402c882aec
Guilherme da Silva <zguilhermedev@gmail.com>
2022-03-14 23:37:30 -0300

    implementa validacao do blacklist

--- src/main/java/com/desafiomobi/service/BlackListService.java
+++ src/main/java/com/desafiomobi/service/BlackListService.java
@@ -1,13 +1,14 @@
 package com.desafiomobi.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import com.desafiomobi.domain.Colaborador;
 import com.desafiomobi.domain.dto.ColaboradorBlackListDTO;
 
 @Service
@@ -15,23 +16,25 @@ public class BlackListService {
 
 	RestTemplate restTemplate = new RestTemplate();
 
-	private final Logger logger = LoggerFactory.getLogger(BlackListService.class);
-
 	final static String BLACK_LIST_URL = "https://5e74cb4b9dff120016353b04.mockapi.io/api/v1/blacklist";
 
-	public Colaborador findColaboradorInTheBlackListByCpf(String cpf) {
+	public ColaboradorBlackListDTO findColaboradorInTheBlackListByCpf(String cpf) {
 
 		UriComponents builder = UriComponentsBuilder.fromHttpUrl(BLACK_LIST_URL).queryParam("search", cpf).build();
 
 		ColaboradorBlackListDTO[] response = restTemplate.getForObject(builder.toUriString(), ColaboradorBlackListDTO[].class);
-
-		logger.info(response.toString());
+		
+		List<ColaboradorBlackListDTO> list = new ArrayList<>(Arrays.asList(response));
+		
+		if(!list.isEmpty()) {
+			return list.get(0);
+		}
 		
 		return null;
 	}
 	
 	public boolean validateIfColaboradorIsInBlackList(String cpf) {
-		Colaborador colaborador = findColaboradorInTheBlackListByCpf(cpf);
+		ColaboradorBlackListDTO colaborador = findColaboradorInTheBlackListByCpf(cpf);
 		
 		if(colaborador != null ) {
 			return true;
